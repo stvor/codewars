f9b2511d15fbd60a62a9cfc3a8af31d35b95b229
Stas Voronin <jameslebron@yandex.ru>
2022-10-06 23:05:57 +0300

    counting-dublicates

---
+++ counting-dublicates.js
@@ -0,0 +1,33 @@
+// Instructions
+// https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/javascript
+
+// Solution
+function duplicateCount(text){
+  const lowerCased = text.toLowerCase();
+  let result = 0;
+  const obj = {};
+  
+  for (let char of lowerCased) {
+    if (!obj[char]) {
+      obj[char] = 1;
+    } else {
+      obj[char] += 1;
+    }
+  }
+  
+  for (let count of Object.values(obj)) {
+    if (count > 1) {
+      result += 1;
+    }
+  }
+
+  return result;
+}
+
+// Tests
+console.log(duplicateCount(""), 0);
+console.log(duplicateCount("abcde"), 0);
+console.log(duplicateCount("aabbcde"), 2);
+console.log(duplicateCount("aabBcde"), 2,"should ignore case");
+console.log(duplicateCount("Indivisibility"), 1);
+console.log(duplicateCount("Indivisibilities"), 2, "characters may not be adjacent");
