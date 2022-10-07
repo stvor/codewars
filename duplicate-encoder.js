ed2427d61b797e5ab76e800197a74a05de2306fc
Stas Voronin <jameslebron@yandex.ru>
2022-10-07 20:55:30 +0300

    duplicate-encoder

---
+++ duplicate-encoder.js
@@ -0,0 +1,28 @@
+// Instructions
+// https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/javascript
+
+// Solution
+function duplicateEncode(word){
+  const lowerCasedArr = word.toLowerCase().split("");
+  const obj = {};
+  
+  for (let char of lowerCasedArr) {
+    if (!obj[char]) {
+      obj[char] = 1;
+    } else {
+      obj[char] += 1;
+    }
+  }
+  
+  let result = lowerCasedArr
+    .map((char) => obj[char] == 1 ? "(" : ")")
+    .join("");
+  
+  return result;
+}
+
+// Tests
+console.log(duplicateEncode("din"),"(((");
+console.log(duplicateEncode("recede"),"()()()");
+console.log(duplicateEncode("Success"),")())())","should ignore case");
+console.log(duplicateEncode("(( @"),"))((");
